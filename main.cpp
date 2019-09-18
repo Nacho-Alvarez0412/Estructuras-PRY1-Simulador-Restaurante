2d03d19a4c8ba920e38cf7f4d70a5b1973385a16
Ignacio Alvarez <52050956+Nacho-Alvarez0412@users.noreply.github.com>
2019-09-18 17:17:35 -0600

    Actualizado

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 
 #include <QApplication>
 #include <QString>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
+    cout<< "Harto de esta mierda";
     return 0;
 }
